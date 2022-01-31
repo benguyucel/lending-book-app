9cd8187f4cc062876487c5156d9901392683d46a
benguyucel <yucelbengu8@gmail.com>
2022-01-31 23:37:21 +0300

    65465

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({
     extended: true
 })) // for parsing application/x-www-form-urlencoded
-app.use(cors());
+app.use(cors(
+    {
+        origin:"*"
+    }
+));
 app.use(helmet());
 const appErrorHandler = require('./middleware/errorHandler');
 let bookRoute = require('./routes/bookRouter')
@@ -17,13 +21,6 @@ let publisherRoute = require('./routes/publisherRouter')
 let personRoute = require('./routes/personRouter')
 const authorRoute = require('./routes/authorRoutes')
 const lendingRouter = require('./routes/lendingRouter')
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 app.use("/book", bookRoute)
 app.use("/author", authorRoute)
 app.use("/publisher", publisherRoute)
